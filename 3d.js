1d372c68f89bf9fe9557b939ad7a76dce7cd2fcf
yichuanhao <1274816963@qq.com>
2023-04-21 14:13:36 +0800

    add status

--- 3d.js
+++ 3d.js
@@ -1,7 +1,5 @@
 import "./style.css";
 import { ConvexGeometry } from "three/addons/geometries/ConvexGeometry.js";
-import { FontLoader } from "three/addons/loaders/FontLoader.js";
-import { TextGeometry } from "three/addons/geometries/TextGeometry.js";
 import data from "./color/colors.csv";
 import firstLevel from "./color/firstLevel.csv";
 import secondLevel from "./color/secondLevel.csv";
@@ -36,17 +34,6 @@ const color_convex_mapped = color_convex.map((d) => {
 // console.log(data.filter(d=>d.subparent==="棕"))
 let colorsData = data;
 extend([labPlugin]);
-const mySelect = document.querySelector("#mySelect");
-mySelect.add(new Option("全部"));
-let op = [];
-data.forEach((d) => {
-  op.push(d.subparent);
-});
-op = Array.from(new Set(op));
-op.forEach((d) => {
-  mySelect.add(new Option(d));
-});
-const al = color_convex_mapped.filter((d) => d.color === "酱色");
 
 const meshSelect = document.querySelector("#meshSelect");
 const meshMatselect = document.querySelector("#meshMatSelect");
@@ -63,7 +50,6 @@ meshSelect.add(new Option("显示全部边界"));
 
 meshMatselect.addEventListener("change", onMeshMatChangeOption);
 meshSelect.addEventListener("change", onMeshChangeOption);
-mySelect.addEventListener("change", onChangeOption);
 // const colorsData = data
 // console.log(data)
 const pointer = new THREE.Vector2();
@@ -126,47 +112,6 @@ const MeshHuang = new THREE.Mesh(boundaryHuang, mhuang);
 const MeshBai = new THREE.Mesh(boundaryBai, mbai);
 const MeshHei = new THREE.Mesh(boundaryHei, mhei);
 scene.add(MeshQing, MeshChi, MeshHuang, MeshBai, MeshHei);
-const group = new THREE.Group();
-// colorsData.forEach((d, i) => {
-//   const c = colord({ l: d.l, a: d.a, b: d.b }).toHex();
-//   const sprite1 = new THREE.Sprite(new THREE.SpriteMaterial({ color: c }));
-//   const x = map(d.l, 0, 100, 0, 1, true);
-//   const y = map(d.a, -128, 127, 0, 1, true);
-//   const z = map(d.b, -128, 127, 0, 1, true);
-//   sprite1.position.set(x, y, z);
-//   sprite1.scale.set(0.05, 0.05, 0.05);
-//   sprite1.colorIndex = i;
-//   group.add(sprite1);
-// });
-//
-
-// al.forEach((d, i) => {
-//   const c = colord({ l: d.l, a: d.a, b: d.b }).toHex();
-//   const sprite1 = new THREE.Sprite(new THREE.SpriteMaterial({ color: c }));
-//   const x = map(d.l, 0, 100, 0, 1, true);
-//   const y = map(d.a, -128, 127, 0, 1, true);
-//   const z = map(d.b, -128, 127, 0, 1, true);
-//   sprite1.position.set(x, y, z);
-//   sprite1.scale.set(0.05, 0.05, 0.05);
-//   sprite1.colorIndex = i;
-//   group.add(sprite1);
-// });
-
-scene.add(group);
-//add BufferGeometry
-const g = new THREE.BufferGeometry();
-
-// const arra = al.map((e) => {
-//   return new THREE.Vector3(parseFloat(e.l), parseFloat(e.a), parseFloat(e.b));
-// });
-
-// const testG = new ConvexGeometry(arra);
-// const testM = new THREE.MeshBasicMaterial({
-//   color: 0x00ff00,
-//   wireframe: false,
-// });
-// const testMesh = new THREE.Mesh(testG, testM);
-// scene.add(testMesh);
 
 const size = {
   width: window.innerWidth,
@@ -183,8 +128,11 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(size.width, size.height);
-scene.add(new THREE.AxesHelper(1.5));
+// 坐标轴
+let axisHelper = new THREE.AxesHelper(1.5);
+scene.add(axisHelper);
 scene.background = new THREE.Color(0x555555);
+
 // onMeshChangeOption()
 animate();
 
@@ -200,9 +148,10 @@ function render() {
   renderer.render(scene, camera);
 }
 
+// 创建控制器
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.addEventListener("change", render); // use if there is no animation loop
-controls.minDistance = 2;
+controls.minDistance = 1;
 controls.maxDistance = 10;
 controls.target.set(0, 0, -0.2);
 controls.update();
@@ -215,9 +164,7 @@ function onWindowResize() {
 }
 
 let selectedObject = null;
-
 function onPointerMove(event) {
-  group.clear();
   setInfo("", "", "", "", "", "");
   if (!app.group) return;
   if (selectedObject) {
@@ -248,27 +195,6 @@ function onPointerMove(event) {
         res.object.name,
         res.object.from ? res.object.from : ""
       );
-      if (res.object.name) {
-        var text = res.object.name; // 文字内容
-        var fontLoader = new FontLoader(); // 创建字体加载器
-        fontLoader.load("fonts/SJgzks_Regular.json", function (font) {
-          // 加载字体文件
-          var geometry = new TextGeometry(text, {
-            // 创建文字几何体
-            font: font,
-            size: 0.03,
-            height: 0.0,
-            curveSegments: 1,
-            bevelEnabled: false,
-          });
-          var material = new THREE.MeshBasicMaterial({ color: 0xffffff }); // 创建材质
-          var mesh = new THREE.Mesh(geometry, material); // 创建网格
-          mesh.position.set(res.point.x, res.point.y, res.point.z); // 设置位置
-          mesh.rotateY(45);
-          group.add(mesh);
-          scene.add(group); // 将网格添加到场景中
-        });
-      }
     }
   }
 }
@@ -281,106 +207,7 @@ function setInfo(name, l, a, b, colorName, from) {
   document.querySelector("#colorName").innerHTML = colorName;
   document.querySelector("#from").innerHTML = from;
 }
-// 下拉框切换
-function onChangeOption(d) {
-  const index = d.srcElement.selectedIndex;
-  const val = d.srcElement.options[index].text;
-  if (index === 0) {
-    colorsData = data;
-  } else {
-    colorsData = data.filter((d) => d.subparent === val);
-  }
-  group.clear();
-  const arr = [];
-  colorsData.forEach((d, i) => {
-    const c = colord({ l: d.l, a: d.a, b: d.b }).toHex();
-    const x = map(d.l, 0, 100, 0, 1, true);
-    const y = map(d.a, -128, 127, 0, 1, true);
-    const z = map(d.b, -128, 127, 0, 1, true);
-    let position = new THREE.Vector3(x, y, z);
-    // 存入每个点坐标位置
-    arr.push(position);
-    // 创建球体
-    const convexSphereGeo = new THREE.SphereGeometry(0.01);
-    const convexSphereMat = new THREE.MeshBasicMaterial({
-      color: c,
-    });
-    const convexSphere = new THREE.Mesh(convexSphereGeo, convexSphereMat);
-    convexSphere.position.copy(position);
-    scene.add(convexSphere);
-  });
-  // 创建凹凸几何体
-  var convexGeo = new ConvexGeometry(arr);
-  let sss = [];
-  // 根据顶点点位逆转为lab
-  convexGeo.attributes.position.array.forEach((i) => {
-    sss.push(i);
-  });
-  const size = 3; // 每个子数组的长度
-  const result = [];
-  // 每3个拆成二维数组，取出每个坐标对应的lab值
-  for (let i = 0; i < sss.length; i += size) {
-    result.push(sss.slice(i, i + size));
-  }
-  let colors = [];
-  // 每个lab值转换成着色器需要的格式
-  result.forEach((i) => {
-    let l = map(i[0], 0, 1, 0, 100, true);
-    let a = map(i[1], 0, 1, -128, 127, true);
-    let b = map(i[2], 0, 1, -128, 127, true);
-    let c = colord({ l: l, a: a, b: b }).toHex();
-    colors = colors.concat(colorRgb(c));
-  });
 
-  // 给凹凸几何体增加color属性
-  convexGeo.setAttribute("color", new THREE.Float32BufferAttribute(colors, 3));
-  // 着色器
-  var material = new THREE.ShaderMaterial({
-    vertexShader: `
-      varying vec2 vUv;
-      varying vec3 vColor;
-      attribute vec3 color;
-      void main() {
-          vColor = color;
-          vUv = uv;
-          gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-      }
-  `,
-    fragmentShader: `
-      varying vec2 vUv;
-      varying vec3 vColor;
-      void main() {
-        gl_FragColor=vec4(vColor, 1.0); //最后设置顶点颜色，点与点之间会自动插值
-      }
-  `,
-  });
-  // 创建一个网格，将凹凸体和GLSL着色器结合起来
-  var cube = new THREE.Mesh(convexGeo, material);
-  scene.add(cube);
-}
-
-function colorRgb(sColor) {
-  sColor = sColor.toLowerCase();
-  //十六进制颜色值的正则表达式
-  var reg = /^#([0-9a-fA-f]{3}|[0-9a-fA-f]{6})$/;
-  // 如果是16进制颜色
-  if (sColor && reg.test(sColor)) {
-    if (sColor.length === 4) {
-      var sColorNew = "#";
-      for (var i = 1; i < 4; i += 1) {
-        sColorNew += sColor.slice(i, i + 1).concat(sColor.slice(i, i + 1));
-      }
-      sColor = sColorNew;
-    }
-    //处理六位的颜色值
-    var sColorChange = [];
-    for (var i = 1; i < 7; i += 2) {
-      sColorChange.push(parseInt("0x" + sColor.slice(i, i + 2)) / 255);
-    }
-    return sColorChange;
-  }
-  return sColor;
-}
 function onMeshChangeOption(d) {
   const index = d.srcElement.selectedIndex;
   const val = d.srcElement.options[index].text;
@@ -459,6 +286,7 @@ let app = new Vue({
       secondColorValue: [], // 第二层级选中的值
       thirdColorValue: [], // 第三层级选中的值
       group: null, // 用于管理mesh对象
+      status: false,
     };
   },
   methods: {
@@ -577,7 +405,7 @@ let app = new Vue({
         let a = map(i[1], 0, 1, -128, 127, true);
         let b = map(i[2], 0, 1, -128, 127, true);
         let c = colord({ l: l, a: a, b: b }).toHex();
-        colors = colors.concat(colorRgb(c));
+        colors = colors.concat(this.colorToRgb(c));
       });
 
       // 给凹凸几何体增加color属性
@@ -585,9 +413,10 @@ let app = new Vue({
         "color",
         new THREE.Float32BufferAttribute(colors, 3)
       );
-      // 着色器
-      var material = new THREE.ShaderMaterial({
-        vertexShader: `
+      if (!this.status) {
+        // 着色器
+        var material = new THREE.ShaderMaterial({
+          vertexShader: `
           varying vec2 vUv;
           varying vec3 vColor;
           attribute vec3 color;
@@ -597,16 +426,27 @@ let app = new Vue({
               gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
           }
       `,
-        fragmentShader: `
+          fragmentShader: `
           varying vec2 vUv;
           varying vec3 vColor;
           void main() {
             gl_FragColor=vec4(vColor, 1.0); //最后设置顶点颜色，点与点之间会自动插值
           }
       `,
-      });
-      // 创建一个网格，将凹凸体和GLSL着色器结合起来
-      return new THREE.Mesh(convexGeo, material);
+        });
+        // 创建一个网格，将凹凸体和GLSL着色器结合起来
+        return new THREE.Mesh(convexGeo, material);
+      } else {
+        // 给凹凸几何体增加color属性
+        convexGeo.setAttribute(
+          "color",
+          new THREE.Float32BufferAttribute(colors, 3)
+        );
+        const testM = new THREE.MeshBasicMaterial({
+          wireframe: true,
+        });
+        return new THREE.Mesh(convexGeo, testM);
+      }
     },
     // 处理数据给下拉框
     formatData(data) {
@@ -639,6 +479,12 @@ let app = new Vue({
         return sColorChange;
       }
       return sColor;
+    },
+    statusChange() {
+      if (this.group.children.length > 0) this.group.clear();
+      this.firstLevelChange(this.firstColorValue, true);
+      this.secondLevelChange(this.secondColorValue, true);
+      this.thirdLevelChange(this.thirdColorValue, true);
     },
   },
   created() {
